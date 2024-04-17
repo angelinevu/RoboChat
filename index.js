955ecde0a46542f0231c9a06dbc985f630601a88
Angeline Vu <angelineevu@gmail.com>
2024-04-17 15:11:40 -0700

    basic routes

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "jsx")
+
+const port = 3000;
+
+app.get("/", (req, res) => {                    // Home Page
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("*", (req, res) => {                    // Invalid
+    res.status(404).send("404 - Not Found");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, "0.0.0.0", () => {             // Local host
+    console.log("Server running at https://localhost:3000/");
+});
