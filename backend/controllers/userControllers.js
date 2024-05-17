edca3e5de12d333b763c238ee6a2b3b53658042d
Angeline Vu <angelineevu@gmail.com>
2024-05-17 15:07:29 -0700

    frontend folder

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,12 +1,14 @@
-//User sidebar
 import User from "../models/userModel.js"
+
+//Get Users for sidebar DRAFT
 
 export const getUsersForSidebar = async (req, res) => {
     try {
 
-        const loggedInUserId = req.user._id
+        const loggedInUserId = req.user._id     //Signed in user
 
-        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password")
+        //CHANGE: select all other users (do not select password)
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password") 
 
         return res.status(200).json(filteredUsers)
     } catch (error) {
