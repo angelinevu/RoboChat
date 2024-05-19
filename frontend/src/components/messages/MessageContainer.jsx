f3dab7a50582f23a2b9a06c8d5ea84556cb7ecbd
Zainub Siddiqui <zainub2@pdx.edu>
2024-05-19 15:37:34 -0700

    get conversations done

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Messages from './Messages';
 import MessageInput from './MessageInput';
 import { TiMessages } from "react-icons/ti";
+import useConversation from '../../zustand/useConversation';
 
 const MessageContainer = () => {
-  const noChatSelected = false; // if false, shows messages
+  const {selectedConversation, setSelectedConversation} = useConversation()
+
+  useEffect(() => {
+    // cleanup function 
+    return () => setSelectedConversation(null)
+  }, [setSelectedConversation])
+
   return (
     <div className='md:min-w-[450px] flex flex-col'>
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
@@ -15,7 +22,7 @@ const MessageContainer = () => {
           {/*<div className='bg-slate-200 px-4 py-2 mb-2'>*/}
           <div className= 'px-4 py-2 mb-2'>
             <span className='label-text text-gray-900'>To: </span>
-            <span className='text-gray-900 font-bold'> Jane Doe</span>
+            <span className='text-gray-900 font-bold'> {selectedConversation.fullName}</span>
           </div>
           <Messages />
           <MessageInput />
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import useConversation from '../../zustand/useConversation'
 
-const Conversation = () => {
+const Conversation = ({conversation, lastIdx}) => {
+  const { selectedConversation, setSelectedConversation } = useConversation()
+
+  const isSelected = selectedConversation?._id === conversation._id
+
   return <>
-  <div className='flex gap-2 items-center hover:bg-blue-500 rounded p-2 py-1 cursor-pointer'>
+  <div className={`flex gap-2 items-center hover:bg-blue-500 rounded p-2 py-1 cursor-pointer
+  ${isSelected ? "bg-blue-500": ""}
+  `}
+    onClick={() => setSelectedConversation(conversation)}
+  >
     <div className='avatar'>
         <div className='w-16 rounded-full bg-gray-800'>
-            <img src='https://robohash.org/zainub2' alt='user avatar'/>
+            <img 
+            src={conversation.pic} 
+            alt='user avatar'/>
         </div>
     </div>
 
     <div className='flex flex-col flex-1'>
        <div className='flex flex-col flex-1'>
-        <p className='font-bold text-black'>Jane Doe</p>
+        <p className='font-bold text-black'>{conversation.fullName}</p>
         </div> 
     </div>
   </div>
 
-  <div className='divider my-0 py-0 h-1'/>
+  {!lastIdx && <div className='divider my-0 py-0 h-1'/>}
   </>
 }
 
