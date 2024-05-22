8e9d96e1555884803edee2b8609afec44b8f3743
Angeline Vu <angelineevu@gmail.com>
2024-05-21 20:10:02 -0700

    gc button

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { IoSearchSharp } from 'react-icons/io5';
+import { MdOutlineGroup } from "react-icons/md";
 import useConversation from '../../zustand/useConversation'
 import useGetConversation from '../../hooks/useGetConversations'
 import toast from 'react-hot-toast'
@@ -25,12 +26,15 @@ const SearchInput = () => {
   }
   return (
     <form onSubmit={handleSubmit} className='flex items-center gap-4'>
-      <input type='text' placeholder='Search' className='font-mono input input-bordered rounded-full text-lg px-7 py-2'
+      <input type='text' placeholder='Search User' className='font-mono input input-bordered rounded-full text-lg px-7 py-2'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
       <button type='submit' className='btn btn-circle bg-blue-500 text-white'>
         <IoSearchSharp className='w-6 h-6 outline-none' />
+      </button>
+      <button type='submit' className='btn btn-circle bg-green-500 text-white'>
+        <MdOutlineGroup className='w-6 h-6 outline-none' />
       </button>
     </form>
   );
